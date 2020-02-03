3918b5c4b1aa923ad8e16bea5b3390246e19f1f0
Paul Gierz <pgierz@awi.de>
2020-02-03 08:07:42 +0100

    empty echam example files for testing.

--- tests/test_esm_analysis.py
+++ tests/test_esm_analysis.py
@@ -1,3 +1,11 @@
+# @Author: Paul Gierz <pgierz>
+# @Date:   2019-12-18T11:23:36+01:00
+# @Email:  pgierz@awi.de
+# @Filename: test_esm_analysis.py
+# @Last modified by:   pgierz
+# @Last modified time: 2020-02-03T06:55:33+01:00
+
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -20,15 +28,14 @@ class TestEsm_analysis(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
-
     def test_command_line_interface(self):
         """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
-        assert 'Console script for' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
+        assert "Console script for" in result.output
+        help_result = runner.invoke(cli.main, ["--help"])
         assert help_result.exit_code == 0
-        assert ('--help' in help_result.output) and ('Show this message and exit.' in help_result.output)
+        assert ("--help" in help_result.output) and (
+            "Show this message and exit." in help_result.output
+        )
